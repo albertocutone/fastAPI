9d9910c6cf84398d19c19a84c1d956b482b63546
alberto <alberto.cutone@gmail.com>
2022-05-08 13:39:07 +0200

    Adding HTTP status codes

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,5 @@
 from operator import ne
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 import schemas
 import models
 from database import engine, SessionLocal
@@ -17,7 +17,7 @@ def get_db():
 	finally:
 		db.close()
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db : Session = Depends(get_db)):
 	new_blog = models.Blog(title=request.title,body=request.body)
 	db.add(new_blog)
@@ -30,7 +30,10 @@ def all(db: Session = Depends(get_db)):
 	blogs = db.query(models.Blog).all()
 	return blogs
 
-@app.get('/blog/{id}')
-def show(id, db: Session = Depends(get_db)):
+@app.get('/blog/{id}',status_code=200)
+def show(id, response: Response, db: Session = Depends(get_db)):
 	blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+	if not blog:
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+							detail=f"Blog with the id {id} is not available")
 	return blog
